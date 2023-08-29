2a9c6a643e6abfb14849c291e4a898480cb73d0b
Roy <roywashington1547@gmail.com>
2023-08-28 21:26:35 -0500

    init with landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,79 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button';
+import TextTransition from '@/components/textTransition';
+import {
+  FaRegClock,
+  FaHandsHelping,
+  FaCode,
+  FaGithub,
+  FaTwitter,
+} from 'react-icons/fa';
 
-export default function Home() {
+const LandingPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="m-4">
+      <div className="grid gap-2 text-center rounded-lg shadow-lg p-14 place-items-center ring-1 ring-gray-300 bg-zinc-200 bg-[url('../../public/card-bg.svg')]">
+        <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 md:text-6xl lg:text-8xl">
+          apt-chat.
+        </h1>
+        <div className="flex font-sans text-2xl italic text-center text-black">
+          a chatbot for &#8202;
+          <TextTransition />
+        </div>
+        <a href="https://tally.so/r/wbj6RZ">
+          <Button className="mt-2">join the waitlist.</Button>
+        </a>
+      </div>
+      <div className="flex flex-col gap-4 mt-6 md:flex-row">
+        <div className="w-full p-12 shadow-lg rounded-xl md:w-1/3 bg-zinc-200 ring-1 ring-gray-300">
+          <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 md:text-5xl">
+            <FaRegClock size={48} className="mb-6" />
+            Always Current.
+          </h1>
+          <p className="text-lg leading-7 [&:not(:first-child)]:mt-6">
+            Keeps up with latest API and framework changes.
+          </p>
+        </div>
+        <div className="w-full p-12 shadow-lg rounded-xl md:w-1/3 bg-zinc-200 ring-1 ring-gray-300">
+          <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 md:text-5xl">
+            <FaHandsHelping size={48} className="mb-6" />
+            Helps When ChatGPT Can&apos;t.
+          </h1>
+          <p className="text-lg leading-7 [&:not(:first-child)]:mt-6">
+            Stop worrying about knowledge cutoffs and extreme hallucination.
+          </p>
+        </div>
+        <div className="w-full p-12 shadow-lg rounded-xl md:w-1/3 bg-zinc-200 ring-1 ring-gray-300">
+          <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 md:text-5xl">
+            <FaCode size={48} className="mb-6" />
+            APIs, Frameworks, and More.
+          </h1>
+          <p className="text-lg leading-7 [&:not(:first-child)]:mt-6">
+            Get help with anything from React to Python to Java.
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+      <footer className="mt-32 text-left rounded-md w-fit">
+        <div className="container">
+          <div className="flex space-x-4">
+            <a
+              href="https://github.com/your-github-username"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={24} />
+            </a>
+            <a
+              href="https://twitter.com/your-twitter-handle"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter size={24} />
+            </a>
+          </div>
+          <p className="mt-2 text-xs font-thin">© 11_0000_1000_CORP</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+export default LandingPage;
---
+++ src/components/textTransition.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import TextTransition, { presets } from 'react-text-transition';
+
+const TEXTS = ['developers.', 'programmers.', 'engineers.'];
+
+const TextTransitionComponent: React.FC = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((prevIndex) => prevIndex + 1),
+      3000
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+
+  return (
+    <TextTransition
+      springConfig={presets.wobbly}
+      className="text-2xl font-semibold tracking-tight scroll-m-20 "
+    >
+      {TEXTS[index % TEXTS.length]}
+    </TextTransition>
+  );
+};
+
+export default TextTransitionComponent;
