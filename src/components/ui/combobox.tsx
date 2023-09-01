5d60a2a0177cf57c8f06905a7f1793eed6872683
Roy <roywashington1547@gmail.com>
2023-09-01 12:37:53 -0500

    full implementtion of chat

---
+++ src/components/ui/combobox.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import * as React from 'react';
+import { CaretSortIcon, CheckIcon } from '@radix-ui/react-icons';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from '@/components/ui/command';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+
+const frameworks = [
+  {
+    value: 'next.js',
+    label: 'Next.js',
+  },
+  {
+    value: 'sveltekit',
+    label: 'SvelteKit',
+  },
+  {
+    value: 'nuxt.js',
+    label: 'Nuxt.js',
+  },
+  {
+    value: 'remix',
+    label: 'Remix',
+  },
+  {
+    value: 'astro',
+    label: 'Astro',
+  },
+];
+
+export function Combobox() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState('');
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? frameworks.find((framework) => framework.value === value)?.label
+            : ' Chat Focus'}
+          <CaretSortIcon className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search framework..." className="h-9" />
+          <CommandEmpty>No framework found.</CommandEmpty>
+          <CommandGroup>
+            {frameworks.map((framework) => (
+              <CommandItem
+                key={framework.value}
+                onSelect={(currentValue) => {
+                  setValue(currentValue === value ? '' : currentValue);
+                  setOpen(false);
+                }}
+              >
+                {framework.label}
+                <CheckIcon
+                  className={cn(
+                    'ml-auto h-4 w-4',
+                    value === framework.value ? 'opacity-100' : 'opacity-0'
+                  )}
+                />
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+// `PopoverContent` contains the commands and options the user can interact with.
+//      - `CommandInput` allows for a future search implementation.
+//      - `CommandEmpty` displays a message when no frameworks are found (not implemented).
+//      - `CommandGroup` contains individual `CommandItem` components, each representing a framework.
+//
+//  Each `CommandItem` updates the `value` state when selected, which in turn updates the displayed value on the `Button`.
+//
+// ## Customization
+// 1. `frameworks`: You can update this array to include other frameworks or options.
+// 2. `Button` Styles: You can modify the `Button` component's `variant` and `className` for different styling.
+// 3. Search functionality: The `CommandInput` placeholder suggests search functionality that you can implement.
+// 5. No Results Behavior: You could implement actual functionality for `CommandEmpty` to display when no frameworks match a search query.
+// This code is written in TypeScript and assumes that all imported components and utilities are correctly installed and imported.
